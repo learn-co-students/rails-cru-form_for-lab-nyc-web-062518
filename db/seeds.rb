b8428b12929529735815986a06be252b5435c6d7
Daniel Ran <danielr1735@gmail.com>
2018-07-22 13:35:43 -0400

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Artist.create(name: "Michael Jackson", bio: "born in the USA.")
+Artist.create(name: "Yoni Rechter", bio: "born in Israel")
+Artist.create(name: "Ravi Shankar", bio: "born in India.")
+
+Genre.create(name: "world music")
+Genre.create(name: "funk")
+Genre.create(name: "classical")
+Genre.create(name: "pop")
+Genre.create(name: "soul")
+Genre.create(name: "rock")
+Genre.create(name: "indian")
